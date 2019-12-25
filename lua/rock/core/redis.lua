542d62666bbc7d72002bf5fa648e3c7289c92858
jufeng <zhaojun@300.cn>
2019-12-25 15:49:32 +0800

    add mysql local config

--- lua/rock/core/redis.lua
+++ lua/rock/core/redis.lua
@@ -9,41 +9,44 @@
 local require = require
 local ngx = ngx
 local redis = require "resty.redis"
-local rock_core = require "rock.core"
+local log = require "rock.core.log"
 local setmetatable = setmetatable
+local config = require("rock.core.config")
 
 local _M = {}
 local mt = {
     __index = _M
 }
 function _M.new()
+    local config =  config.local_conf()
+    local redis_config = config.redis
     local red = redis:new()
-    red:set_timeouts(1000, 1000, 2000) -- 1 sec red:set_timeouts(connect_timeout, send_timeout, read_timeout)
-    local ok, err = red:connect("127.0.0.1", 6379)
+    red:set_timeout(redis_config.timeout) -- 1 sec red:set_timeouts(connect_timeout, send_timeout, read_timeout)
+    local ok, err = red:connect(redis_config.host, redis_config.port)
     if not ok then
-        rock_core.log.error("redis failed to connect: ".. err)
+        log.error("redis failed to connect: ".. err)
         return nil
     end
 
     local self = {
-        red = red
+        red = red,
+        redis_config = redis_config
     }
     return  setmetatable(self,mt)
 end
 
 function _M:close()
-    local ok ,err = self.red:set_keepalive(30000, 30)
+    local ok ,err = self.red:set_keepalive(self.redis_config.max_idle_timeout, self.redis_config.pool_size)
     if not ok then
-        rock_core.log.error("redis failed to set_keepalive: ".. err)
+        log.error("redis failed to set_keepalive: ".. err)
         _M.new() -- 重新初始化
     end
-
 end
 
 function _M:subscribe(key)
     local res, err = self.red:subscribe(key)
     if not res then
-        rock_core.log.error(" failed to subscribe: ".. err)
+        log.error(" failed to subscribe: ".. err)
     ---subscribe("dog")
     ---  正常返回 ["subscribe","dog",1]
     end
@@ -54,7 +57,7 @@ end
 function _M:publish(key,value_str)
     local res, err = self.red:publish(key,value_str)
     if not res then
-        rock_core.log.error("failed to subscribe: " .. err)
+        log.error("failed to subscribe: " .. err)
         ---   red2:publish("dog", "Hello")
     end
     self:close()
@@ -64,7 +67,7 @@ end
 function _M:read_reply()
     local res, err = self.red:read_reply()
     if not res then
-        rock_core.log.error(" failed to read reply: " .. err)
+        log.error(" failed to read reply: " .. err)
         ---  正常返回 ["message","dog","Hello"], 取得时候，取最后一个Hello ，
     end
     self:close()
