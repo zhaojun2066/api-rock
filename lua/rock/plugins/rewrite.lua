67d04f32bb2e830b262a817eac5abf05a994d710
jufeng <zhaojun@300.cn>
2020-01-03 11:05:26 +0800

    add uri rewrite

---
+++ lua/rock/plugins/rewrite.lua
@@ -0,0 +1,85 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: 86186
+-- Date: 3/1/2020
+-- Time: 上午10:14
+-- url 重定向
+--
+
+local ngx = ngx
+local require = require
+local rock_core = require "rock.core"
+local ngx_re = ngx.re
+local pairs = pairs
+
+
+
+local schema = {
+    type = "object",
+    properties = {
+        uri_reg = {type = "string"},
+        to_uri = {type = "string"},
+        code = {type = "integer"},
+        vars = {type = "object"}
+    },
+    required = {"uri_reg", "to_uri", "code"}
+}
+---vars  可以是ngx.var 里的key value
+local _M = {
+    version = "1,0",
+    name = "rewrite"
+}
+
+local function match_vars(vars)
+    local status
+    for k,v in pairs(vars) do
+        if not ngx.var[k] then
+            status = 1
+        end
+    end
+    return status
+end
+local function check_conf(conf)
+
+    local code = conf.code
+    if not code then
+        return nil,"router's rewrite param code null"
+    end
+
+    local to_uri = conf.to_uri
+    if not to_uri then
+        return nil,"router's rewrite param to_uri null"
+    end
+    return true,nil
+end
+
+function _M.rewrite(conf)
+    local res,err = check_conf(conf)
+    if not res then
+        return rock_core.response.exit_msg(400,err)
+    end
+
+    local uri_reg = conf.uri_reg
+    local vars = conf.vars
+    local code = conf.code
+    local to_uri = conf.to_uri
+    local request_uri = ngx.var.uri
+
+    if uri_reg and not vars then
+        local match_group = ngx_re.match(request_uri,uri_reg)
+        if match_group then
+            return ngx.redirect(to_uri,code)
+        end
+    elseif not uri_reg and vars then
+        if  match_vars(vars) then
+            return ngx.redirect(to_uri,code)
+        end
+    elseif uri_reg and vars then
+        local match_group = ngx_re.match(request_uri,uri_reg)
+        if match_vars(vars) and match_group then
+            return ngx.redirect(to_uri,code)
+        end
+    end
+end
+
+return _M
